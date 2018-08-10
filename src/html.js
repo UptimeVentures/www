1cf5728c2f82cd037892533f0f72755cddf1259e
Nicholas Young <nyoung@uptime.ventures>
2018-08-10 12:26:10 -0600

    chore(lint): remove Helmet

--- src/html.js
+++ src/html.js
@@ -6,35 +6,23 @@
  */
 
 import React from 'react'
-import Helmet from 'react-helmet'
 
 export default function HTML(props) {
-  const {
-    headComponents,
-    body,
-    postBodyComponents,
-  } = props
-
-  const { title, meta, link } = Helmet.rewind()
-
   return (
-    <html lang="en">
+    <html {...props.htmlAttributes}>
       <head>
         <meta charSet="utf-8" />
         <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0 maximum-scale=5.0" />
-        {headComponents}
         <link rel="author" href="/humans.txt"/>
-        {title.toComponent()}
-        {meta.toComponent()}
-        {link.toComponent()}
+        {...props.headComponents}
       </head>
-      <body>
+      <body {...props.bodyAttributes}>
         <div
           id="___gatsby"
-          dangerouslySetInnerHTML={{ __html: body }}
+          dangerouslySetInnerHTML={{ __html: props.body }}
         />
-        {postBodyComponents}
+        {props.postBodyComponents}
       </body>
     </html>
   )
